c5eb822de86bf69e134feaa84ccb7ab91f1fcade
KCMicheal <egwunwokekc@gmail.com>
2022-01-24 11:16:11 +0100

    perf: migrations created

---
+++ BioticApi/Controllers/BioticUsersController.cs
@@ -0,0 +1,20 @@
+﻿using BioticApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BioticApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BioticUsersController : ControllerBase
+    {
+        private readonly BioticDbContext _context;
+        public BioticUsersController(BioticDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public 
+    }
+}
